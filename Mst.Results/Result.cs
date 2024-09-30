dcb37afe2c73069f6ced0189c680de38fd9adba5
Mehran Sattary <mehran999m9@gmail.com>
2024-09-30 12:33:14 +0330

    Add project files.

---
+++ Mst.Results/Result.cs
@@ -0,0 +1,87 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Mst.Results;
+
+public class Result
+{
+    public bool IsFailed { get; set; }
+    public bool IsSuccess { get; set; }
+
+    private readonly List<string> _errors;
+
+    private readonly List<string> _successes;
+
+    public Result()
+    {
+        IsSuccess = true;
+        IsFailed = false;
+        _errors = new List<string>();
+        _successes = new List<string>();
+    }
+
+    [JsonIgnore]
+    public IReadOnlyList<string> Errors => _errors;
+
+    [JsonIgnore]
+    public IReadOnlyList<string> Successes => _successes;
+
+    public void AddErrorMessage(string message)
+    {
+        message = String.Fix(message);
+
+        if (string.IsNullOrEmpty(message) || _errors.Contains(message)) return;
+
+        _errors.Add(message);
+        IsFailed = true;
+        IsSuccess = false;
+    }
+
+    public void RemoveErrorMessage(string message)
+    {
+        message = String.Fix(message);
+
+        if (string.IsNullOrEmpty(message)) return;
+
+        _errors.Remove(message);
+
+        if (_errors.Count == 0)
+        {
+            IsFailed = false;
+            IsSuccess = true;
+        }
+    }
+
+    public void ClearErrorMessages()
+    {
+        _errors.Clear();
+        IsFailed = false;
+        IsSuccess = true;
+    }
+
+    public void AddSuccessMessage(string message)
+    {
+        message = String.Fix(message);
+
+        if (string.IsNullOrEmpty(message) || _successes.Contains(message)) return;
+
+        _successes.Add(message);
+    }
+
+    public void RemoveSuccessMessage(string message)
+    {
+        message = String.Fix(message);
+
+        if (string.IsNullOrEmpty(message)) return;
+
+        _successes.Remove(message);
+    }
+
+    public void ClearSuccessMessages() => _successes.Clear();
+
+    public void ClearAllMessages()
+    {
+        ClearErrorMessages();
+        ClearSuccessMessages();
+    }
+
+}
---
+++ Mst.Results/ResultExtensions.cs
@@ -0,0 +1,67 @@
+﻿namespace Mst.Results;
+
+public static class ResultExtensions
+{
+    static ResultExtensions()
+    {
+    }
+
+    public static Result ConvertToMstResult(this FluentResults.Result result)
+    {
+        Result mstResult = new()
+        {
+            IsFailed = result.IsFailed,
+            IsSuccess = result.IsSuccess,
+        };
+
+        if (result.Errors != null)
+        {   
+            foreach (var item in result.Errors)
+            {
+                mstResult.AddErrorMessage(message: item.Message);
+            }
+        }
+
+        if (result.Successes != null)
+        {
+            foreach (var item in result.Successes)
+            {
+                mstResult.AddSuccessMessage(message: item.Message);
+            }
+        }
+
+        return mstResult;
+    }
+
+    public static Result<T> ConvertToMstResult<T>(this FluentResults.Result<T> result)
+    {
+        Result<T> mstResult = new()
+        {
+            IsFailed = result.IsFailed,
+            IsSuccess = result.IsSuccess,
+        };
+
+        if (result.IsFailed == false)
+        {
+            mstResult.Value = result.Value;
+        }
+
+        if (result.Errors != null)
+        {
+            foreach (var item in result.Errors)
+            {
+                mstResult.AddErrorMessage(message: item.Message);
+            }
+        }
+
+        if (result.Successes != null)
+        {
+            foreach (var item in result.Successes)
+            {
+                mstResult.AddSuccessMessage(message: item.Message);
+            }
+        }
+
+        return mstResult;
+    }
+}
